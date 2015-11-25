101ef7d09b005ad279840c5e76c630c8553562a5
Abhijeet De <abhijeet3192@gmail.com>
2015-11-26 01:00:30 +0530

    Added node-yelp

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/ads-dev'
+    uri: 'mongodb://localhost/nightlife'
   },
 
-  seedDB: true
+  seedDB: false
 };
